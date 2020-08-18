5c94d456bffacf02767be45016c7ffc0529a8843
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-08-17 22:28:04 -0400

    Initial commit

---
+++ HuzzahPay Merchant/HuzzahPay Merchant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HuzzahPay Merchant
+//
+//  Created by Apurva Deshmukh on 8/17/20.
+//  Copyright © 2020 HuzzahPay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
